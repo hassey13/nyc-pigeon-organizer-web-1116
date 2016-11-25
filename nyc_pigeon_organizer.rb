2ebbfbcccb1b25adb99cbfa6a6a0304c356b11e9
Eric Hasselbring <ehasselbring@gmail.com>
2016-11-25 17:44:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
-def nyc_pigeon_organizer(data)
+def nyc_pigeon_organizer(array)
   # write your code here!
+      pigeon_hash = {}
+      array.each do |attribute, list|
+           list.each do |secondary, names|
+                 secondary = secondary.to_s
+               names.each do |name|
+                   if !pigeon_hash[name]
+                      pigeon_hash[name] = {}
+                   end
+                   if !pigeon_hash[name][attribute]
+                       pigeon_hash[name][attribute] = [secondary]
+                   else
+                      pigeon_hash[name][attribute] << secondary
+                   end
+               end
+           end
+      end
+      return pigeon_hash
 end
